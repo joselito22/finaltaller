a316dd097a3dcfd51e320bcf1f94a41e735e2c6b
joselito22 <joseandressantosgomez@gmail.com>
2022-12-01 18:10:38 -0500

    add new file

---
+++ Tallerprog.py
@@ -0,0 +1,78 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+
+#########################################
+#LOGIN STANDARD USER
+
+driver = webdriver.Chrome(executable_path='chromedriver.exe')
+driver.get("https://www.saucedemo.com/")
+
+elem = driver.find_element(By.ID, "user-name")
+elem.clear()
+elem.send_keys("standard_user")
+
+elem = driver.find_element(By.ID, "password")
+elem.clear()
+elem.send_keys("secret_sauce")
+
+boton = driver.find_element(By.ID, "login-button")
+boton.click();
+time.sleep(3)
+
+resultado = driver.find_element(By.XPATH, "//*[@class='header_secondary_container']/span")
+assert resultado.text == 'PRODUCTS'
+time.sleep(3)
+
+driver.close()
+
+#########################################
+#LOCKED OUT USER
+
+driver = webdriver.Chrome(executable_path='chromedriver.exe')
+driver.get("https://www.saucedemo.com/")
+
+elem = driver.find_element(By.ID, "user-name")
+elem.clear()
+elem.send_keys("locked_out_user")
+
+elem = driver.find_element(By.ID, "password")
+elem.clear()
+elem.send_keys("secret_sauce")
+
+boton = driver.find_element(By.ID, "login-button")
+boton.click();
+
+time.sleep(5)
+
+resultado = driver.find_element(By.XPATH, "//*[@id='login_credentials']/h4")
+assert resultado.text == 'Accepted usernames are:'
+time.sleep(3)
+
+driver.close()
+
+
+#########################################
+#PROBLEM USER
+
+driver = webdriver.Chrome(executable_path='chromedriver.exe')
+driver.get("https://www.saucedemo.com/")
+
+elem = driver.find_element(By.ID, "user-name")
+elem.clear()
+elem.send_keys("problem_user")
+
+elem = driver.find_element(By.ID, "password")
+elem.clear()
+elem.send_keys("secret_sauce")
+
+boton = driver.find_element(By.ID, "login-button")
+boton.click();
+time.sleep(3)
+
+resultado = driver.find_element(By.XPATH, "//*[@class='header_secondary_container']/span")
+assert resultado.text == 'PRODUCTS'
+time.sleep(3)
+
+driver.close()
